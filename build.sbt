cc6cd7ca778a503c658dc103a191150b37990166
vedprakash-singh <vedprak22@gmail.com>
2017-04-20 18:38:43 +0530

    Add project structure

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := """scala-play-examples"""
+
+version := "1.0-SNAPSHOT"
+
+scalaVersion := "2.11.7"
+
---
+++ src/main/scala/Hello.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by Ved Singh on 4/20/2017.
+  */
+object Hello {
+  def main(args: Array[String]) {
+
+    print("Welcome")
+  }
+
+}
